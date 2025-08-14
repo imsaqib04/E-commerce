5b03ec5e39aa0815cc273971c31b709f3a8e95b5
saqib <saqibansari2011@gmail.com>
2025-08-14 14:37:36 +0530

    done

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -21,5 +21,8 @@ from shop import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    #shop app
     path("",views.index,name="index"),
+    path("<int:id>",views.detail,name="detail"),
+    path('checkout/',views.checkout,name='checkout'),
 ]
